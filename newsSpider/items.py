a3ccbd5264f172c1ae70b5c986c91c0ef840f5f5
liuhoward <liuhoward09@gmail.com>
2016-09-04 16:07:52 -0400

    1. crawler for indeed.com, claimed profile

--- newsSpider/items.py
+++ newsSpider/items.py
@@ -12,25 +12,14 @@ class NewsspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     company = scrapy.Field()
-    reviewID = scrapy.Field()
+    Headquarters = scrapy.Field()
 
-    reviewRating = scrapy.Field()
-    workRating = scrapy.Field()
-    compensationRating = scrapy.Field()
-    securityRating = scrapy.Field()
-    managementRating = scrapy.Field()
-    cultureRating = scrapy.Field()
+    Revenue = scrapy.Field()
+    Employees = scrapy.Field()
+    Industry = scrapy.Field()
+    Links = scrapy.Field()
 
-    reviewTitle = scrapy.Field()
-    jobTitle = scrapy.Field()
-    jobLocation = scrapy.Field()
-    reviewDate = scrapy.Field()
-    text = scrapy.Field()
-    pros = scrapy.Field()
-    cons = scrapy.Field()
 
-    upVoteCount = scrapy.Field()
-    downVoteCount = scrapy.Field()
 
 
 
--- newsSpider/spiders/CNNSpider.py
+++ newsSpider/spiders/CNNSpider.py
@@ -28,9 +28,9 @@ class ExampleSpider(CrawlSpider):
 
         domain = "http://www.indeed.com"
 
-        for comLink in soup.find_all("a", attrs={"data-tn-element": "rating-link"}):
-            reviewLink = domain + comLink['href']
-            yield scrapy.Request(reviewLink, callback=self.parse_news)
+        for comLink in soup.find_all("a", attrs={"data-tn-element": "title-link"}):
+            titleLink = domain + comLink['href']
+            yield scrapy.Request(titleLink, callback=self.parse_news)
 
 
 
@@ -44,122 +44,20 @@ class ExampleSpider(CrawlSpider):
 
     def parse_news(self,response):
 
-        #print "======================================================"
-        #print(response.url)
-
         soup = BeautifulSoup(response.body, 'lxml')
-
-        content = soup.find("div", id = "cmp-content")
 
         companyName = soup.find("h2", itemprop="name")
         if companyName and companyName.string:
             company = companyName.string
-        else:
 
-            company = response.url.strip().split("/")[4].strip().replace('-', ' ')
-
-        firstPage = False
-        if "start=" in response.url.strip():
-            firstPage = False
-        else:
-            firstPage = True
-
-        #if firstPage:
-        #    headContainer = content.find("div", id = "cmp-reviews-header-container")
-        #    headReviewContainer = content.find("div", id="cmp-review-container")
-        #    parse_review(headReviewContainer)
-
-        index = 1
-        for container in content.find_all("div", class_ = "cmp-review-container"):
-
-            if (not firstPage) and (index == 1):
-                index += 1
-                continue
-            else:
-                item = NewsspiderItem()
-                ret = self.get_review(container, company, item)
-                index += 1
-                yield item
-
-
-
-        domain = "http://www.indeed.com"
-        nextlink=soup.find("a", attrs={"data-tn-element": "next-page"})
-        if nextlink:
-            link=domain + nextlink['href']
-            #print "==================================link " + link
-            yield scrapy.Request(link, callback=self.parse_news, dont_filter=True)
-
-
-
-    def get_review(self, container, company, item):
-
+        item = NewsspiderItem()
         item['company'] = company
 
-        reviewID = container.find("div", attrs={"data-tn-entitytype": "reviewId"})
-        if reviewID:
-            item['reviewID'] = reviewID['data-tn-entityid']
+        sidebar = soup.find("dl", id = "cmp-company-details-sidebar")
+        if sidebar:
+            #dtlist = []
+            #ddlist = []
+            for dt in sidebar.find_all("dt"):
+                item[dt.string] = dt.next_sibling.string
 
-        reviewRating = container.find("meta", itemprop="ratingValue")
-        if reviewRating:
-            item['reviewRating'] = float(reviewRating['content'])
-
-        tbody = container.find("table", class_="cmp-ratings-expanded")
-
-        aspects = []
-        for innerRating in tbody.find_all("span", class_="cmp-rating-inner rating"):
-            tmp = innerRating['style']
-
-            val = tmp[tmp.find(":") + 1:tmp.find("px")]
-            aspects.append(float(val) / 17.2)
-
-        if len(aspects) != 5:
-            print "================================number of aspects is wrong"
-
-        item['workRating'] = aspects[0]
-        item['compensationRating'] = aspects[1]
-        item['securityRating'] = aspects[2]
-        item['managementRating'] = aspects[3]
-        item['cultureRating'] = aspects[4]
-
-        titleTmp = container.find("div", class_ = "cmp-review-title")
-        reviewTitle = titleTmp.find("span", itemprop = "name")
-        item['reviewTitle'] = reviewTitle.get_text().strip()
-
-        jobTitle = container.find("span", class_ = "cmp-reviewer-job-title")
-        item['jobTitle'] = jobTitle.get_text().strip()
-
-        jobLocation = container.find("span", class_ = "cmp-reviewer-job-location")
-        item['jobLocation'] = jobLocation.get_text().replace("-", "").strip()
-
-        reviewDate = container.find("span", class_ = "cmp-review-date-created")
-        item['reviewDate'] = reviewDate.get_text().strip()
-
-        text = container.find("span", class_ = "cmp-review-text")
-        item['text'] = text.get_text().strip()
-
-        pros = container.find("div", class_ = "cmp-review-pro-text")
-        if pros:
-            item['pros'] = pros.get_text().strip()
-        else:
-            item['pros'] = ""
-
-        cons = container.find("div", class_ = "cmp-review-con-text")
-        if cons:
-            item['cons'] = cons.get_text().strip()
-        else:
-            item['cons'] = ""
-
-        reviewVote = container.find("span", class_="cmp-review-vote")
-        for vote in reviewVote.find_all("span", class_="cmp-vote-count"):
-            if vote['name'] == "upVoteCount":
-                if vote.string:
-                    item['upVoteCount'] = float(vote.string.strip())
-                else:
-                    item['upVoteCount'] = 0
-            elif vote['name'] == "downVoteCount":
-                if vote.string:
-                    item['downVoteCount'] = float(vote.string.strip())
-                else:
-                    item['downVoteCount'] = 0
-
+        return item
